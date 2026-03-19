dd283b48192cb7ef4eefbf0576e086ff1e2f177d
SageSeeger <c28sage.seeger@afacademy.af.edu>
2026-03-18 21:38:27 -0600

    theCommit

--- src/TDM4.vhd
+++ src/TDM4.vhd
@@ -54,52 +54,45 @@ use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 
 entity TDM4 is
-	generic ( constant k_WIDTH : natural  := 4); -- bits in input and output
-    Port ( i_clk		: in  STD_LOGIC;
-           i_reset		: in  STD_LOGIC; -- asynchronous
-           i_D3 		: in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
-		   i_D2 		: in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
-		   i_D1 		: in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
-		   i_D0 		: in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
-		   o_data		: out STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
-		   o_sel		: out STD_LOGIC_VECTOR (3 downto 0)	-- selected data line (one-cold)
-	);
+    generic ( k_WIDTH : natural := 4); -- removed "constant" (cleaner)
+    Port ( i_clk    : in  STD_LOGIC;
+           i_reset  : in  STD_LOGIC; -- asynchronous
+           i_D3     : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+           i_D2     : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+           i_D1     : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+           i_D0     : in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+           o_data   : out STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
+           o_sel_n  : out STD_LOGIC_VECTOR (3 downto 0) -- one-cold
+    );
 end TDM4;
 
 architecture behavioral of TDM4 is
 
-	signal   f_sel		 : unsigned(1 downto 0)	:= "00"; -- 2 bit counter output to select MUX input
-	
-begin	
-	
-	-- PROCESSES ----------------------------------------
-	
-	-- 2 Bit counter Process ----------------------------
-	-- counter rolls over automatically
-	-- asynchronous reset to "00"
-	twoBitCounter_proc : process(i_clk, i_reset)
-	begin
-		if i_reset = '1' then
-			f_sel <= "00";
-		elsif rising_edge(i_clk) then
-			f_sel <= f_sel + 1;
-		end if;
-	end process twoBitCounter_proc;
-	-----------------------------------------------------
-	
+    signal f_sel : unsigned(1 downto 0) := "00";
 
-	-- CONCURRENT STATEMENTS ----------------------------
-	
-	-- output MUXs
-	o_DATA <= i_D3 when f_sel = "11" else
-			  i_D2 when f_sel = "10" else
-			  i_D1 when f_sel = "01" else
-			  i_D0;
-			  
-	o_SEL  <=  "0111" when f_sel = "11" else
-			   "1011" when f_sel = "10" else
-			   "1101" when f_sel = "01" else
-			   "1110";
-		
+begin
+
+    -- 2-bit counter
+    process(i_clk, i_reset)
+    begin
+        if i_reset = '1' then
+            f_sel <= "00";
+        elsif rising_edge(i_clk) then
+            f_sel <= f_sel + 1;
+        end if;
+    end process;
+
+    -- MUX output
+    o_data <= i_D3 when f_sel = "11" else
+              i_D2 when f_sel = "10" else
+              i_D1 when f_sel = "01" else
+              i_D0;
+
+    -- one-cold select
+    o_sel_n <= "0111" when f_sel = "11" else
+               "1011" when f_sel = "10" else
+               "1101" when f_sel = "01" else
+               "1110";
+
 end behavioral;
 
--- src/TDM4_tb.vhd
+++ src/TDM4_tb.vhd
@@ -44,65 +44,87 @@
 --|
 --+----------------------------------------------------------------------------
 library ieee;
-  use ieee.std_logic_1164.all;
-  use ieee.numeric_std.all;
-  
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
 entity TDM4_tb is
 end TDM4_tb;
 
 architecture test_bench of TDM4_tb is 	
-  
-	component TDM4 is
-		-- fill in from TDM4.vhd
 
-	end component TDM4;
+    component TDM4 is
+        generic ( k_width : natural := 4);
+        Port ( i_clk    : in  STD_LOGIC;
+               i_reset  : in  STD_LOGIC;
+               i_D3     : in  STD_LOGIC_VECTOR (k_width - 1 downto 0);
+               i_D2     : in  STD_LOGIC_VECTOR (k_width - 1 downto 0);
+               i_D1     : in  STD_LOGIC_VECTOR (k_width - 1 downto 0);
+               i_D0     : in  STD_LOGIC_VECTOR (k_width - 1 downto 0);
+               o_data   : out STD_LOGIC_VECTOR (k_width - 1 downto 0);
+               o_sel_n  : out STD_LOGIC_VECTOR (3 downto 0)
+        );
+    end component;
 
-	-- Setup test clk (20 ns --> 50 MHz) and other signals
-	
-	-- Constants
-	constant k_IO_WIDTH : natural := 4;
-	-- Signals
-	
+    -- Clock
+    constant k_clock_period : time := 20 ns;
+
+    -- Constants
+    constant k_IO_WIDTH : natural := 4;
+
+    -- Signals
+    signal w_clk   : std_logic := '0';
+    signal w_reset : std_logic := '0';
+
+    signal w_D3, w_D2, w_D1, w_D0 : std_logic_vector(k_IO_WIDTH-1 downto 0);
+
+    signal w_data  : std_logic_vector(k_IO_WIDTH-1 downto 0);
+    signal w_sel_n : std_logic_vector(3 downto 0);
+
 begin
-	-- PORT MAPS ----------------------------------------
-	-- map ports for any component instances (port mapping is like wiring hardware)
-	uut_inst : TDM4 
-	generic map ( k_WIDTH =>  )
-	port map ( 
-       i_clk   => 
-       i_reset => 
-       i_D3    => 
-       i_D2    => 
-       i_D1    => 
-       i_D0    => 
-       o_data  => 
-       o_sel   => 
-	);
-	-----------------------------------------------------	
-	
-	-- PROCESSES ----------------------------------------	
-	-- Clock Process ------------------------------------
-	clk_process : process
-	begin
 
+    -- DUT
+    uut_inst : TDM4 
+    generic map ( k_width => k_IO_WIDTH )
+    port map ( 
+        i_clk   => w_clk,
+        i_reset => w_reset,
+        i_D3    => w_D3,
+        i_D2    => w_D2,
+        i_D1    => w_D1,
+        i_D0    => w_D0,
+        o_data  => w_data,
+        o_sel_n => w_sel_n
+    );
 
+    -- Clock process
+    clk_process : process
+    begin
+        while true loop
+            w_clk <= '0';
+            wait for k_clock_period/2;
+            w_clk <= '1';
+            wait for k_clock_period/2;
+        end loop;
+    end process;
 
-	end process clk_process;
-	-----------------------------------------------------	
-	
-	-- Test Plan Process --------------------------------
-	test_process : process 
-	begin
-		-- assign test values to data inputs
+    -- Test process
+    test_process : process 
+    begin
+        -- assign test values to data inputs
+           w_D3 <= "1000";
+           w_D2 <= "0100";
+           w_D1 <= "0010";
+           w_D0 <= "0001";
 
-				
-		-- reset the system first
-		w_reset <= '1';
-		wait for k_clk_period;		
-		w_reset <= '0';
-		
-		wait; -- let the TDM do its work
-	end process;	
-	-----------------------------------------------------	
-	
+        -- reset
+        w_reset <= '1';
+        wait for k_clock_period;
+        w_reset <= '0';
+
+        -- let it run
+        wait for 160 ns;
+
+        wait;
+    end process;
+
 end test_bench;
